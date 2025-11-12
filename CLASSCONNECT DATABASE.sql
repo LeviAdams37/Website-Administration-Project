d259ae99d9248ae00be438f93628abe2110433ed
williamsondustin3-design <williamsondustin3@gmail.com>
2025-11-12 09:06:54 -0600

    Add files via upload

---
+++ CLASSCONNECT DATABASE.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE classconnect;
+USE classconnect;
+
+CREATE TABLE users (
+	ID INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
